80d114acbf1b49942a98439c632f76ba94747d34
MForofontov <mykyta.forofontov@hotmail.com>
2024-11-01 11:32:17 +0000

    feat: FIxed some tests

--- mathFunctions/isOdd.ts
+++ mathFunctions/isOdd.ts
@@ -2,15 +2,15 @@
  * Checks if a number is odd.
  * 
  * @param n - The number to check.
- * @returns True if the number is odd, false if it is even or if the input is not an integer.
- * @throws Will throw an error if n is NaN.
+ * @returns True if the number is odd, false if it is even.
+ * @throws Will throw an error if n is NaN or if n is not an integer.
  */
 export function isOdd(n: number): boolean {
     if (isNaN(n)) {
         throw new Error('Input must be a number');
     }
     if (!Number.isInteger(n)) {
-        return false;
+        throw new Error('Input must be an integer');
     }
     return n % 2 !== 0;
 }
