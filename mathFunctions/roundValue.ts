e7419b3fcedac693a5727f51ad6a1e1d022bf452
MForofontov <mykyta.forofontov@hotmail.com>
2024-11-01 11:48:32 +0000

    feat: FIxed some tests and functions

--- mathFunctions/roundValue.ts
+++ mathFunctions/roundValue.ts
@@ -3,8 +3,12 @@
  * 
  * @param n - The number to round.
  * @returns The number rounded to the nearest integer.
+ * @throws Will throw an error if n is NaN.
  */
 export function roundValue(n: number): number {
+    if (isNaN(n)) {
+        throw new Error('Input must be a number');
+    }
     return Math.round(n);
 }
 
