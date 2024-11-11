0e7a70d0515ca2f3abe01cca2f84527eec6114d9
MForofontov <mykyta.forofontov@hotmail.com>
2024-11-11 10:25:29 +0000

    feat: Created new unitests for geometicFunctions
    Co-authored-by: MalsoDarms <mykyta_forofontov@hotmail.com>

--- mathFunctions/geometricFunctions/degreesToRadians.ts
+++ mathFunctions/geometricFunctions/degreesToRadians.ts
@@ -3,8 +3,12 @@
  * 
  * @param degrees - The value in degrees.
  * @returns The value in radians.
+ * @throws Will throw an error if degrees is NaN.
  */
 export function degreesToRadians(degrees: number): number {
+    if (isNaN(degrees)) {
+        throw new Error('Degrees must be a number');
+    }
     return degrees * (Math.PI / 180);
 }
 
