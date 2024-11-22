95e1158b7a8c5c5ba1107cf3db4e98ee69a7f866
MForofontov <mykyta.forofontov@hotmail.com>
2024-11-22 14:13:03 +0000

    GITHUB-5 feat: Added new getStartOfWeek

--- dateFunctions/getStartOfYear.ts
+++ dateFunctions/getStartOfYear.ts
@@ -5,6 +5,9 @@
  * @returns A new Date object representing the start of the year.
  */
 export function getStartOfYear(date: Date): Date {
+    if (isNaN(date.getTime())) {
+        throw new Error('Invalid date');
+    }
     return new Date(date.getFullYear(), 0, 1);
 }
 
