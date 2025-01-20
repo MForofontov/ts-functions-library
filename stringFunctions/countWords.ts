b87412e046bd624dabbac5944e7f653e85fe9af6
MForofontov <mykyta.forofontov@hotmail.com>
2025-01-20 17:28:59 +0000

    GITHUB-5 fix: fixed countWords

--- stringFunctions/countWords.ts
+++ stringFunctions/countWords.ts
@@ -5,6 +5,9 @@
  * @returns The number of words in the string.
  */
 export function countWords(str: string): number {
+    if (str.trim() === '') {
+        return 0;
+    }
     return str.trim().split(/\s+/).length;
 }
 
