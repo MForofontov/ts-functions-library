f50b2d9e9baab3e6b43a0d58642eb28bfb744b2c
MForofontov <mykyta.forofontov@hotmail.com>
2025-01-24 11:52:11 +0000

    GITHUB-5 fix: fixed findLongestWord

--- stringFunctions/findLongestWord.ts
+++ stringFunctions/findLongestWord.ts
@@ -6,11 +6,13 @@
  */
 export function findLongestWord(str: string): string {
     // Remove punctuation and special characters
-    const cleanedStr = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()"\t\n\r\b\f\v\\\'\"]/g, '');
-    return cleanedStr.split(' ').reduce((longest, current) => {
+    const cleanedStr = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()"\t\n\r\b\f\v\\\'\"]/g, ' ');
+    
+    // Split the string into words using any whitespace character as the delimiter
+    return cleanedStr.split(/\s+/).reduce((longest, current) => {
         return current.length > longest.length ? current : longest;
     }, '');
 }
 
 // Example usage:
-// console.log(findLongestWord("The quick brown fox jumps over the lazy dog.")); // "quick"
+// console.log(findLongestWord("The quick brown fox jumps over the lazy dog.")); // "jumps"
