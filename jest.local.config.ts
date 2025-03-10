53a427501e99c08ace600dcf38f1cb9152f39e2f
MForofontov <mykyta.forofontov@hotmail.com>
2025-03-10 22:34:14 +0000

    chore: update jest configuration output paths for local testing

--- jest.local.config.ts
+++ jest.local.config.ts
@@ -5,9 +5,9 @@ module.exports = {
   testEnvironment: "node", // Change to standard node environment
   reporters: [
     "default", // default jest reporter
-    ['jest-html-reporter', { outputPath: 'local_test/jest.html' }], // jest html reporter
+    ['jest-html-reporter', { outputPath: 'local-testing/jest.html' }], // jest html reporter
   ],
   collectCoverage: true,
-  coverageDirectory: 'local_test/coverage',
+  coverageDirectory: 'local-testing/coverage',
   coverageReporters: ['json', 'lcov', 'text', 'clover'],
 };
