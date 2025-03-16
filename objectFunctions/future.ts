e36a371d318b69ab22ed394a2a2bc6614775e58f
MForofontov <mykyta.forofontov@hotmail.com>
2025-03-16 16:09:49 +0000

    test: reorganize and expand tests for safeSet function to improve coverage

--- objectFunctions/future.ts
+++ objectFunctions/future.ts
@@ -1,21 +1,4 @@
-
-// safeSet.ts
-  export function safeSet<T extends Record<string, any>>(
-    obj: T,
-    path: string,
-    value: any
-  ): void {
-    const keys = path.split('.');
-    let current: any = obj;
-    while (keys.length > 1) {
-      const key = keys.shift()!;
-      if (!(key in current)) current[key] = {};
-      current = current[key];
-    }
-    current[keys[0]] = value;
-  }
-  
-  // keyBy.ts
+// keyBy.ts
   export function keyBy<T extends Record<string, any>>(
     array: T[],
     key: keyof T
