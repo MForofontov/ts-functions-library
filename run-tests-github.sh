e94b59bc49b749a0a207962eff7ebff8deef6dab
MForofontov <mykyta.forofontov@hotmail.com>
2025-03-24 14:19:47 +0000

    refactor: improve logging and error handling in run-tests.sh and run-tests-github.sh

--- run-tests-github.sh
+++ run-tests-github.sh
@@ -13,6 +13,12 @@ log_with_time "[INFO] Current local time: $(date)"
 # Print the current time in UTC
 log_with_time "[INFO] Current time in UTC: $(date -u)"
 
+# Check if Allure is installed
+if ! command -v allure >/dev/null 2>&1; then
+    log_with_time "[ERROR] Allure is not installed. Please install it and try again."
+    exit 1
+fi
+
 # Check if Jest is installed
 if ! command -v jest >/dev/null 2>&1; then
     log_with_time "[ERROR] Jest is not installed. Please install it and try again."
@@ -25,17 +31,10 @@ if ! jest; then
     log_with_time "[WARNING] Jest tests failed, but continuing with the script"
 fi
 
-# Check if Allure is installed
-if ! command -v allure >/dev/null 2>&1; then
-    log_with_time "[ERROR] Allure is not installed. Please install it and try again."
-    exit 1
-fi
-
 # Generate Allure report
 log_with_time "[INFO] Generating Allure report"
 if ! allure generate allure-results --clean -o allure-report; then
     log_with_time "[ERROR] Failed to generate Allure report, but continuing with the script"
-    exit 1
 fi
 
 log_with_time "[INFO] Test execution completed"
